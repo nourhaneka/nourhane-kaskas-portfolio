d035d481689ac35f44adb6d4627f68ee00a710b9
Nourhane kaskas <nourhanekaskas16@gmail.com>
2026-03-03 12:06:43 +0200

    correct the resend problem

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
         const body = await req.json();
 
